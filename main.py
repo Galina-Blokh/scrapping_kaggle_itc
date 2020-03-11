59e3e4fe76eb82712ec203b558dabdacc798a78c
Anna Roitberg <cvi@yandex.ru>
2020-03-11 23:59:43 +0200

    added docstrings

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import csv
 
 
 def create_driver():
+    """
+    create silenium chrome driver
+    :return: driver
+    """
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
@@ -11,6 +15,11 @@ def create_driver():
 
 
 def extract_competitors(driver):
+    """
+    extract number of competitors from competition page
+    :param driver: chrome driver
+    :return: number of competitors
+    """
     try:
         competitors = driver.find_element_by_xpath(
             '//*[@id="site-content"]/div[2]/div/div[2]/div[4]/div[1]/div[2]/p[1]').text
@@ -22,6 +31,11 @@ def extract_competitors(driver):
 
 
 def extract_teams(driver):
+    """
+    extract number of teams from competition page
+    :param driver: chrome driver
+    :return: number of teams
+    """
     try:
         teams = driver.find_element_by_xpath(
             '//*[@id="site-content"]/div[2]/div/div[2]/div[4]/div[1]/div[1]/p[1]').text
@@ -33,6 +47,11 @@ def extract_teams(driver):
 
 
 def extract_header(driver):
+    """
+    extract header from competition page
+    :param driver: chrome driver
+    :return: number of teams
+    """
     try:
         # header new style path
         header_competition = driver.find_element_by_xpath(
@@ -52,6 +71,12 @@ def extract_header(driver):
 
 
 def extract_for_competition(links, driver):
+    """
+    collect data from competition pages
+    :param links: list of links to kaggle competitions pages
+    :param driver: chrome driver
+    :return: list of dictionaries with extracted data
+    """
     competition_info = []
 
     for link in links:
@@ -65,6 +90,12 @@ def extract_for_competition(links, driver):
 
 
 def dicts_to_csv(list_of_dicts, output_filename):
+    """
+    save list of dictionaries to .csv file
+    :param list_of_dicts: list of dictionaries
+    :param output_filename:name for output .csv file
+    :return: none
+    """
     writer = csv.DictWriter(open(output_filename, "w", newline=''), fieldnames=list_of_dicts[0].keys())
     writer.writeheader()
     for row in list_of_dicts:
@@ -72,7 +103,7 @@ def dicts_to_csv(list_of_dicts, output_filename):
 
 
 if __name__ == '__main__':
-    COMPETITION_FEATS = {"get_header": extract_header, "get_teams": extract_teams, "get_compet": extract_competitors}
+    COMPETITION_FEATS = {"header": extract_header, "teams": extract_teams, "competitors": extract_competitors}
     chrome_driver = create_driver()
     competition_data = extract_for_competition(open('competition_links_5p.txt', "r").readlines(), chrome_driver)
     dicts_to_csv(competition_data, 'kaggle_competition.csv')
