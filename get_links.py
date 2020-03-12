730f4a704c300ba1d1704f5bb6364bcc187d6ffc
Anna Roitberg <cvi@yandex.ru>
2020-03-12 21:23:27 +0200

    refactor competition links file

--- get_links.py
+++ get_links.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 import time
 
+
 def get_links_from_page(my_webpage):
+    """
+    get links for competitions from one page
+    :param my_webpage: link to page for scrapping
+    :return: links from one page
+    """
     competition_links = []
     for i in range(1, 20):
         try:
@@ -16,15 +22,25 @@ def get_links_from_page(my_webpage):
 
 
 def connect():
+    """
+    create chrome driver
+    :return: chrome driver
+    """
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(chrome_options=options,executable_path='./chromedriver')
     driver.get("https://www.kaggle.com/search?q=in%3Acompetitions")
-    time.sleep(3)
+    time.sleep(5)
     return driver
 
 
-def get_links_from_site(driver, num_pages=):
+def get_links_from_site(driver, num_pages=5):
+    """
+    get competition links from kaggle site
+    :param driver: chrome driver
+    :param num_pages: number of pages to scrap
+    :return: list of links
+    """
     competition_links = []
     for i in range(num_pages):
         competition_links += get_links_from_page(driver)
@@ -34,8 +50,12 @@ def get_links_from_site(driver, num_pages=):
     return competition_links
 
 
-
 def extract_links_to_file(file_name):
+    """
+    extract links to .txt file
+    :param file_name: output filename
+    :return: nothing
+    """
     kaggle_driver = connect()
     competition_links = get_links_from_site(kaggle_driver)
     output_comp_links = open(file_name, 'w')
@@ -43,5 +63,6 @@ def extract_links_to_file(file_name):
         output_comp_links.write(link + '\n')
     output_comp_links.close()
 
+
 if __name__ == '__main__':
-    extract_links_to_file('competition_links_p.txt')
+    extract_links_to_file('competition_links_5p.txt')
