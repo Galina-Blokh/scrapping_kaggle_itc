74eea8dbba3468c964432592ab4491b156d690fa
GalinaBlokh <galinablokh@gmail.com>
2020-04-04 09:45:38 +0300

    cleaned all to push into the repo fixed function get_num_topics

--- length.py
+++
@@ -1,3 +0,0 @@
-#//*[@id="site-content"]/div[2]/div/div[2]/div[3]/div/div/div/div/div/div[3]/div[2]/span - start
-
-# //*[@id="site-content"]/div[2]/div/div[2]/div[3]/div/div/div/div/div/div[4]/div[2]/span  -end
