b020cf5f4f969068107fbd707d96522906b4b2d8
Anna Roitberg <cvi@yandex.ru>
2020-04-07 17:06:57 +0300

    fix sql script

--- Kaggle_Scrap.sql
+++ Kaggle_Scrap.sql
@@ -1,45 +1,52 @@
-CREATE TABLE 'teams' (
-  'team_id' int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  'name'  varchar(256)
+DROP DATABASE IF EXISTS KaggleITC;
+
+CREATE DATABASE KaggleITC;
+
+USE KaggleITC;
+
+
+CREATE TABLE teams (
+  team_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name  varchar(256)
 );
 
-CREATE TABLE 'competition' (
-  'competition_id' int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  'organizator_name' varchar(256),
-  'description' text,
-  'link' varchar(256),
-  'teams_count' int,
-  'headline' varchar(256),
-  'entries_competition' int,
-  'd_start' datetime,
-  'd_end' datetime,
-  'prize' int,
-  'number_topics' int
+CREATE TABLE competition (
+  competition_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  organizator_name varchar(256),
+  description text,
+  link varchar(256),
+  teams_count int,
+  headline varchar(256),
+  entries_competition int,
+  d_start datetime,
+  d_end datetime,
+  prize int,
+  number_topics int
 );
 
-CREATE TABLE 'leaderboard' (
-  'competition_id' int,
-  'team_id' int,
-  'place_rank' int,
-  'entries_leader' int,
-  'score' float,
-  'position_changed' int
+CREATE TABLE leaderboard (
+  competition_id int,
+  team_id int,
+  place_rank int,
+  entries_leader int,
+  score float,
+  position_changed int
 );
 
-CREATE TABLE 'tags' (
-  'tag_id' int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  'tag' varchar(50)
+CREATE TABLE tags (
+  tag_id int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  tag varchar(50)
 );
 
-CREATE TABLE 'competags' (
-  'tag_id' int,
-  'competition_id' int
+CREATE TABLE competags (
+  tag_id int,
+  competition_id int
 );
 
-ALTER TABLE 'leaderboard' ADD FOREIGN KEY ('competition_key') REFERENCES 'competition' ('competition_id');
+ALTER TABLE leaderboard ADD FOREIGN KEY (competition_id) REFERENCES competition (competition_id);
 
-ALTER TABLE 'leaderboard' ADD FOREIGN KEY ('team_key') REFERENCES 'teams' ('team_id');
+ALTER TABLE leaderboard ADD FOREIGN KEY (team_id) REFERENCES teams (team_id);
 
-ALTER TABLE 'competags' ADD FOREIGN KEY ('tag_key') REFERENCES 'tags' ('tag_id');
+ALTER TABLE competags ADD FOREIGN KEY (tag_id) REFERENCES tags (tag_id);
 
-ALTER TABLE 'competags' ADD FOREIGN KEY ('competition_key') REFERENCES 'competition' ('competition_id');
+ALTER TABLE competags ADD FOREIGN KEY (competition_id) REFERENCES competition (competition_id);
