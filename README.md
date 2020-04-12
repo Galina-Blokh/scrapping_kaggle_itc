ead61c137b1a2b476a67446516b3282c5a0fcdd1
Anna Roitberg <cvi@yandex.ru>
2020-04-12 17:45:23 +0300

    added readme.md, run.sh,  requirments.txt

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # README #
 
-Scraps information about kaggle competitions from kaggle.com
+Scraps information about kaggle competitions from kaggle.com and insert it to database
+
 ### What is this repository for? ###
 
 This is Kaggle parsing project for ITC data mining task
 
 ### How do I get set up? ###
+You need to install Chromedriver Please see https://sites.google.com/a/chromium.org/chromedriver/home
 
-run main.py
+To economy your time you can start from the step 2 and use links from file test_links.txt,
+dor this run ./run.sh 2
+To know more information about run.sh you could run the script without parametrs.
+
+
+Project contains 3 main scripts (steps):
+1) get_links.py  - collects links of kaggle competitions
+2) main.py - collect data about competitions
+3) insert_to_db.py - insertind data to database
+
+For more information you can run one of the scripts with -h
+
 
 ### Contribution guidelines ###
 
 
 ### Who do I talk to? ###
+
 
---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+LINKS_FILE=test_links.txt
+COMPET_FILE=competition.csv
+LEADERS_FILE=leaders.csv
+TAGS_FILE=tags.json
+PASSWORD='' #Your DB password
+
+if [ $# != 1 ]; then
+  echo "This script scrap data from kaggle.com and store it to db."
+  echo "NB If your database required password, please set up in this script."
+  echo "Usage: $0  <start_stage>"
+  echo " e.g.: start_stage 1 - get competiton links, extract data, create db and store to db"
+  echo "       start_stage 2 - extract data, store to db"
+  echo "       start_stage 3 - only create db and store to db"
+  echo "example:$0 2"
+  exit 1;
+fi
+
+stage=$1
+
+if [ $stage -lt 2 ]; then
+    echo "Stage 1"
+    python3 get_links.py --links_file $LINKS_FILE
+fi
+
+if [ $stage -lt 3 ]; then
+    echo "Stage 2"
+    python3 main.py --links_file $LINKS_FILE --compet_file $COMPET_FILE --leader_file $LEADERS_FILE --tags_file $TAGS_FILE
+fi
+
+if [ $stage -lt 4 ]; then
+    echo "Stage 3"
+    mysql -u root --password=$PASSWORD < Kaggle_Scrap.sql
+    python3 insert_to_db.py --compet_file $COMPET_FILE --leader_file $LEADERS_FILE --tags_file $TAGS_FILE --password $PASSWORD
+fi
